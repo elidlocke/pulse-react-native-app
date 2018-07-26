8a1b574fb68a1b7b8453ff0d338d67bb9aba06df
Elizabeth Nenniger <elidlocke@gmail.com>
2018-07-26 11:45:30 -0700

    working footer

--- App.js
+++ App.js
@@ -1,84 +1,29 @@
 import React from 'react';
 import { AppRegistry, StyleSheet, View, Text, Button, ScrollView } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator, BottomTabBar } from 'react-navigation-tabs';
 import { Header, Icon } from 'react-native-elements'
-
-
-class NavBar extends React.Component {
-render() {
-         return (
-             <View style={styles.navContainer}>
-                <View style={styles.navElements}>
-                  <Icon name='newspaper-o' type='font-awesome' color='#9B9B9B' size={36}
-                        onPress={() => navigate('News')} />
-                </View>
-                <View style={styles.navElements}>
-                  <Icon name='heartbeat' type='font-awesome' color='#9B9B9B' size={36}
-                        onPress={() => navigate('Home')} />
-                </View>
-                <View style={styles.navElements}>
-                  <Icon name='calendar-o' type='font-awesome' color='#9B9B9B' size={36}
-                        onPress={() => navigate('Schedule')} />
-                </View>
-                <View style={styles.navElements}>
-                  <Icon name='group' type='font-awesome' color='#9B9B9B' size={36}
-                        onPress={() => navigate('Team')} />
-                </View>
-              </View>
-         )
- }
-}
-
+import styles from './app/styles/style'
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Pulse',
   };
   render() {
-    const { navigate } = this.props.navigation;
     return (
-      <View style={styles.appWrapper}>
-     // MAIN VIEW
-      <ScrollView>
-	    <Button
-		title="Go to News"
-		onPress={() =>
-		  navigate('News')
-		}
-	     />
-
-      </ScrollView>
-      //STICKY NAV BAR @ BOTTOM OF ALL SCREENS
-     <NavBar />
-     </View>
-
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>I am your pulse.</Text>
+      </View>
     );
   }
 }
 
 class NewsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Pulse',
-  };
   render() {
-    const { navigate } = this.props.navigation;
     return (
-      <View style={styles.appWrapper}>
-     // MAIN VIEW
-      <ScrollView>
-	    <Button
-		title="Go Back Home"
-		onPress={() =>
-		  navigate('Home')
-		}
-	     />
-
-      </ScrollView>
-      //STICKY NAV BAR @ BOTTOM OF ALL SCREENS
-     <NavBar />
-     </View>
-
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>I am BIG NEWS</Text>
+      </View>
     );
   }
 }
@@ -88,7 +33,6 @@ class ScheduleScreen extends React.Component {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>I am your schedule</Text>
-     <NavBar />
       </View>
     );
   }
@@ -107,31 +51,47 @@ class TeamScreen extends React.Component {
 
 
 // ALL THE DIFFERENT SCREENS DEFINED
-const NavStack =createStackNavigator({
-  Home: { screen: HomeScreen },
-  News: { screen: NewsScreen },
-  Schedule: { screen: ScheduleScreen },
-  Team: { screen: TeamScreen },
-  NavBar: { screen: NavBar },
-
-});
-
-export default NavStack;
-
-// STYLING HERE
-const styles = StyleSheet.create({
- appWrapper: {
-    flex: 1
+const RootStack =createBottomTabNavigator(
+  {
+  News: NewsScreen,
+  Home: HomeScreen,
+  Schedule: ScheduleScreen,
+  Team: TeamScreen
   },
-  navContainer: {
-    flex: 0.15,
-    flexDirection: 'row'
-  },
-  navElements: {
-    flex: 1,
-    backgroundColor: '#F8F8F9',
-    height: 80,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+	let iconType;
+        if (routeName === 'News') {
+          iconName = 'newspaper-o';
+	  iconType = 'font-awesome';
+        } else if (routeName === 'Home') {
+          iconName = 'heartbeat';
+	  iconType = 'font-awesome';
+        } else if (routeName === 'Schedule') {
+          iconName = 'calendar-o';
+	  iconType = 'font-awesome';
+        } else if (routeName === 'Team') {
+          iconName = 'group';
+	  iconType = 'font-awesome';
+        }
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Icon name={iconName} type={iconType} size={25} color={tintColor} />;
+      },
+    }),
+    initialRouteName: 'Home',
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
---
+++ app/styles/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+ appWrapper: {
+    flex: 1
+  },
+  navContainer: {
+    flex: 0.1,
+    flexDirection: 'row'
+  },
+  navElements: {
+    flex: 1,
+    backgroundColor: '#F8F8F9',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+
